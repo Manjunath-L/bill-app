3d9e73f9343079b2e6e49f3d2b4f9e6ac525b580
Manju-2004 <radhamanjuradhamanju83@gmail.com>
2025-09-08 00:49:35 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import React from "react";
 import { useState } from "react";
 
 const initialFriends = [
@@ -51,7 +50,14 @@ const App = () => {
   }
 
   function handelSpiltBill(value) {
-    console.log(value);
+    setFriends((friends) =>
+      friends.map((friend) =>
+        friend.id === selectedFriend.id
+          ? { ...friend, balance: friend.balance + value }
+          : friend
+      )
+    );
+    setSelectedFriend(null);
   }
   return (
     <div className="app">
